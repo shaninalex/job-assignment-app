0e44e79d9ff6beba615bb762f393254f279bb7e3
shaninalex <shanin.x@gmail.com>
2024-05-09 09:26:27 +0300

    updates

---
+++ backend/server.py
@@ -0,0 +1,4 @@
+# Example server for angular form submittion.
+
+# NOTE FOR FUTURE ME: 
+# please use something simple - starlette or aiohttp. This is just for example
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'multyform';
+    form = this.fb.group({
+        "name": new FormControl("", Validators.required),
+        "skills": this.fb.array<any>([])
+    })
+
+    constructor(
+        private fb: FormBuilder
+    ) { }
+
+    get skills(): FormArray {
+        return this.form.get("skills") as FormArray
+    }
+
+    addSkills() {
+        console.log("add new row")
+        const skillsForm = this.fb.group({
+            "skill": new FormControl("", [Validators.required, Validators.minLength(3)]),
+            "level": new FormControl("", [Validators.required, Validators.minLength(3)]),
+        });
+        this.skills.push(skillsForm);
+    }
+
+    removeSkill(index: number) {
+        console.log("delete row:", index)
+        this.skills.removeAt(index);
+    }
+
+    onSubmit() {
+        console.log("form submitted")
+        console.log(this.form);
+    }
 }
