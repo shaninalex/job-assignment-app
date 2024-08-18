ca6d11480739c0e035bf9d4a8bd11aae9d3935cd
shaninalex <shanin.x@gmail.com>
2024-08-18 16:19:23 +0300

    feat(backend): confirm code table

--- backend/database/models/auth.py
+++ backend/database/models/auth.py
@@ -1,12 +1,13 @@
 import uuid
-from sqlalchemy import text
+from sqlalchemy import text, func
 from sqlalchemy.types import UUID, VARCHAR, Text, Enum
-from .const import AuthStatus
+from .const import AuthStatus, ConfirmStatusCode
 from . import Base
-from .candidate import Candidate
-from .company import CompanyManager
+# from .candidate import Candidate
+# from .company import CompanyManager
+from datetime import datetime
 
-from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy.orm import mapped_column, Mapped
 
 
 class Auth(Base):
@@ -31,6 +32,21 @@ class Auth(Base):
             "status": self.status,
         }
 
-
 # Candidate.auth = relationship("Auth", back_populates="candidate", uselist=False)
 # CompanyManager.auth = relationship("Auth", back_populates="company_manager", uselist=False, cascade="all")
+
+
+class ConfirmCode(Base):
+    __tablename__ = "confirm_codes"
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        server_default=text("uuid_generate_v4()"),
+    )
+    email: Mapped[str] = mapped_column(VARCHAR(100), unique=True)
+    code: Mapped[str] = mapped_column(VARCHAR(6))
+    status: Mapped[AuthStatus] = mapped_column(Enum(ConfirmStatusCode), default=ConfirmStatusCode.SENDED)
+    created_at: Mapped[datetime] = mapped_column(
+        default=func.now(), server_default=func.now(), nullable=True
+    )
--- backend/database/models/const.py
+++ backend/database/models/const.py
@@ -5,3 +5,8 @@ class AuthStatus(enum.Enum):
     ACTIVE = "active"
     BANNED = "banned"
     PENDING = "pending"
+
+
+class ConfirmStatusCode(enum.Enum):
+    SENDED = "sended"
+    USED = "used"
