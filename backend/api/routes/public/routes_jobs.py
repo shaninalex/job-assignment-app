c39ab34f80f907aa771af46582b22804c511a7da
shaninalex <shanin.x@gmail.com>
2024-10-01 10:26:37 +0300

    fix(backend): public positions list, configurations, config dataclass

--- backend/api/routes/public/routes_jobs.py
+++ backend/api/routes/public/routes_jobs.py
@@ -1,6 +1,11 @@
+import logging
 from aiohttp import web
 from pkg import response
 from database import repositories
+from aiohttp_cache import cache
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def setup_jobs_routes(app: web.Application):
@@ -10,10 +15,11 @@ def setup_jobs_routes(app: web.Application):
     app.router.add_get("/api/company/{company}", handle_company_detail)
 
 
+@cache()
 async def handle_positions_list(request: web.Request):
     async with request.app["session"] as session:
         positions = await repositories.get_positions(session) 
-
+    
     return response.success_response({
         "positions": [p.json() for p in positions],
     }, [])
