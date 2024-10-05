b8c6b90d8ccd8015fc0ee86bdbfba4666de26791
shaninalex <shanin.x@gmail.com>
2024-10-05 12:42:06 +0300

    bump: version 0.6.1 → 0.6.2

--- backend/api/routes/utils/utils_routes.py
+++ backend/api/routes/utils/utils_routes.py
@@ -4,7 +4,7 @@ from aiohttp import web
 
 
 async def handle_app_health(request: web.Request):
-    return web.json_response({"status": True, "version": "v0.6.1"}, status=HTTPStatus.OK)
+    return web.json_response({"status": True, "version": "v0.6.2"}, status=HTTPStatus.OK)
 
 
 def setup_utils_routes(app: web.Application):
